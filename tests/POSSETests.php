40ced827a6025d02faaa910d71dfdaea74ebc1c2
Jonny Barnes <jonny@jonnybarnes.net>
2013-12-18 16:02:48 +0000

    Changed order of parameters in assertEquals()

--- tests/POSSETests.php
+++ tests/POSSETests.php
@@ -48,7 +48,7 @@ class POSSETests extends PHPUnit_Framework_TestCase {
 		$actual = $this->p->createTweet("A short tweet", "abc.de", "XXXX");
 		$expected = "A short tweet (abc.de XXXX)";
 
-		$this->assertEquals($actual, $expected);
+		$this->assertEquals($expected, $actual);
 	}
 
 	/**
@@ -61,7 +61,7 @@ class POSSETests extends PHPUnit_Framework_TestCase {
 		$actual = $this->p->createTweet($note, 'abc.de', 'XXXX');
 		$expected = "This is a long note. The following is fluff to fill up. 1234567890123456789012345678901234567890 That was a long… https://abc.de/XXXX";
 
-		$this->assertEquals($actual, $expected);
+		$this->assertEquals($expected, $actual);
 	}
 
 	/**
@@ -74,7 +74,7 @@ class POSSETests extends PHPUnit_Framework_TestCase {
 		$actual = $this->p->createTweet($note, 'abc.de', 'XXXX');
 		$expected = "This is an introductory sentence. We are going to have a link in this note next.\n\nhttps://duckduckgo.com/?q=how+much+wood+could+a+wood-chuck+chuck+if+a+wood+chuck+could+chuck+wood\n\nThen a… https://abc.de/XXXX";
 
-		$this->assertEquals($actual, $expected);
+		$this->assertEquals($expected, $actual);
 	}
 
 	/**
@@ -87,7 +87,7 @@ class POSSETests extends PHPUnit_Framework_TestCase {
 		$actual = $this->p->getTags($note);
 		$expected = array("php", "naive");
 
-		$this->assertEquals($actual, $expected);
+		$this->assertEquals($expected, $actual);
 	}
 
 	/**
@@ -100,6 +100,6 @@ class POSSETests extends PHPUnit_Framework_TestCase {
 		$actual = $this->p->getTags($note);
 		$expected = array("php");
 
-		$this->assertEquals($actual, $expected); 
+		$this->assertEquals($expected, $actual); 
 	}
 }
