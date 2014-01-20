dbbe0f9d02c3423ad8423dab28f7ba0a16a01a42
Jonny Barnes <jonny@jonnybarnes.net>
2014-01-20 16:54:30 +0000

    Got the expected result wrong, d'oh

--- tests/NotePrepTests.php
+++ tests/NotePrepTests.php
@@ -133,7 +133,7 @@ class POSSETests extends PHPUnit_Framework_TestCase {
 	{
 		$note = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est. #longnote";
 		$actual = $this->p->createNote($note, 'abc.de', 'XXXX', 140, true, true);
-		$expected = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum… #longnote https://abc.de/XXXX";
+		$expected = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris… #longnote https://abc.de/XXXX";
 	
 		$this->assertEquals($expected, $actual);
 	}
