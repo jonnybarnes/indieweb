a929711a4a57845db1bbb99b9a4b1ce423d5ed07
Jonny Barnes <jonny@jonnybarnes.net>
2014-01-21 13:45:30 +0000

    Added NewBase60 and NewBase64 functions

--- tests/URLTests.php
+++ tests/URLTests.php
@@ -23,8 +23,71 @@ class POSSETests extends PHPUnit_Framework_TestCase {
 	{
 		$num = 346;
 		$actual = $this->u->numto64($num);
-		$excpected = 'S5';
+		$excpected = '5S';
 
 		$this->assertEquals($excpected, $actual);
+	}
+
+	/**
+	 * Testing reverse of NewBase64
+	 *
+	 */
+	public function testB64ToNum()
+	{
+		$var = '5S';
+		$actual = $this->u->b64tonum($var);
+		$excpected = 346;
+
+		$this->assertEquals($excpected, $actual);
+	}
+
+	/**
+	 * Test both NewBase64 methods at once
+	 *
+	 */
+	public function testBothNewBase64()
+	{
+		$var = 123;
+		$actual = $this->u->b64tonum($this->u->numto64($var));
+
+		$this->assertEquals($var, $actual);
+	}
+
+	/**
+	 * Test NewBase60
+	 *
+	 */
+	public function testNumTo60()
+	{
+		$num = 123;
+		$actual = $this->u->numto60($num);
+		$excpected = '23';
+
+		$this->assertEquals($excpected, $actual);
+	}
+
+	/**
+	 * Test Revers NewBase60
+	 *
+	 */
+	public function testB60ToNum()
+	{
+		$string = '23';
+		$actual = $this->u->b60tonum($string);
+		$excpected = 123;
+
+		$this->assertEquals($excpected, $actual);
+	}
+
+	/**
+	 * Test both NewBase60
+	 *
+	 */
+	public function testBothNewBase60()
+	{
+		$num = 123;
+		$actual = $this->u->b60tonum($this->u->numto60($num));
+
+		$this->assertEquals($num, $actual);
 	}
 }
